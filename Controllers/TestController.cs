9e2c922812b11462fdbd1e39795af4b3ed83d794
A-Stanley <astanley@uplandsoftware.com>
2020-09-10 18:08:09 +0100

    creating controller with json and string

---
+++ Controllers/TestController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EZCourse.Controllers
+{
+    public class TestController : Controller
+    {
+        //different method
+        /*public string StringOut(int id, Person person)
+            {
+               return $"StringOut Action {id} -- {person.FirstName} {person.LastName}";
+            }
+         */
+        /*public IActionResult StringOut(int id, Person person)
+        {
+            return Content($"StringOut Action {id} -- {person.FirstName} {person.LastName}");
+        }
+        */
+        public IActionResult JsonOut(int id, Person person)
+        {
+            var obj = new
+            {
+                Id = id,
+                Person = person
+            };
+            return Json(obj);
+        }
+        public class Person
+        {
+            public string FirstName { get; set;  }
+            public string LastName { get; set; }
+        }
+    }
+}
