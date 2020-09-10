a9bc2fe831961b02ff415b7306e237ca0f29b0b5
A-Stanley <astanley@uplandsoftware.com>
2020-09-11 00:08:58 +0100

    formdata, validation and smtp mail client

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Mvc;
 using EZCourse.Models;
 using EZCourse.Services;
 using Microsoft.Extensions.Options;
+using MailKit.Net.Smtp;
+using MimeKit;
 
 namespace EZCourse.Controllers
 {
@@ -40,18 +42,39 @@ namespace EZCourse.Controllers
 		[HttpPost]
 		public IActionResult Contact(Contact formData)
 		{
+			
 			if (!ModelState.IsValid) 
 			{
 				return View(formData);
 			}
 
-			//1. Do something
+			//1
+			using (var client = new SmtpClient())
+			{
+				client.Connect("mail.domain.com");
+				client.Authenticate("username", "password");
+				var bodyBuilder = new BodyBuilder();
+				bodyBuilder.HtmlBody = $"<p>{formData.Name} ({formData.Email})</p><p>{formData.Phone}</p><p>{formData.Message}</p>";
+				bodyBuilder.TextBody = "{formData.Name} ({formData.Email})\r\n{formData.Phone}\r\n{formData.Message}";
+
+				var message = new MimeMessage();
+				message.Body = bodyBuilder.ToMessageBody();
+				message.From.Add(new MailboxAddress("noreply mysite", "noreply@videotrainingsolutions.com"));
+				message.To.Add(new MailboxAddress("RM", "contact@domain.com"));
+				message.Subject = "Contact Form";
+				client.Send(message);
+
+				client.Disconnect(true);
+			}
+
+			/*1. Do something
 			var htmlBody = $"<p>{formData.Name} ({formData.Email})</p><p>{formData.Phone}</p><p>{formData.Message}</p>";
 			var textBody = "{formData.Name} ({formData.Email})\r\n{formData.Phone}\r\n{formData.Message}";
 
 			_smtpService.SendSingle("Contact Form", htmlBody, textBody,
 									_contactOptions.ContactToName, _contactOptions.ContactToAddress,
 									_contactOptions.ContactFromName, _contactOptions.ContactFromAddress);
+			*/
 
 			//2. Set a message
 			TempData["Message"] = "Thank you! Your message is sent to us.";
